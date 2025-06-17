586e8f57f94dfa519cd7de6a8752df8d8c5350f5
primewk <ouroboroswk@gmail.com>
2025-06-17 23:36:51 +0530

    configured for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static", // 👈 required for static site generation
+  site: 'https://primewk.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
