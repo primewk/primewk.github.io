410d194dac7aa935d486d00e3afdd91ddc0b1aa0
primewk <ouroboroswk@gmail.com>
2025-06-17 22:25:05 +0530

    init

---
+++ src/lib/variables.ts
@@ -0,0 +1,43 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "primewk",
+  rootUrl: "https://zaggonaut.dev",
+  shortDescription: "Student &<br /> Hobbyist Developer",
+  longDescription: "Just a guy who's interested.",
+  
+  // Social media links
+  githubProfile: "https://github.com/primewk",
+  twitterProfile: "https://x.com/primeinspace",
+  linkedinProfile: "https://www.linkedin.com/",
+  
+  // Common text names used throughout the site
+  articlesName: "Articles",
+  projectsName: "Projects",
+  viewAll: "View All",
+  
+  // Common descriptions used throughout the site
+  noArticles: "No featured articles yet.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "My Thoughts & Takes",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription: "A list of my web development projects and developer tools.",
+  projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
+
+  // Profile image
+  profileImage: "pfp.webp",
+
+  // Menu items
+  menu: {
+    home: "/",
+    projects: "/projects",
+    blog: "/blog",
+  }
+};
