60cd898711260d1f912691c592e20f84a0580f42
primewk <ouroboroswk@gmail.com>
2025-06-17 22:48:12 +0530

    changed build output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "static", // 👈 required for static site generation
   vite: {
     plugins: [tailwindcss()],
   },
